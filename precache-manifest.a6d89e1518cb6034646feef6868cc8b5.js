455498ddf601c0cae598f0360bcbe58831acb9a9
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-07-10 18:56:02 +0200

    Updates

---
+++ precache-manifest.a6d89e1518cb6034646feef6868cc8b5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114d77c675002c4efe355c801a9bd9a7",
+    "url": "/eCommerce/index.html"
+  },
+  {
+    "revision": "a1ea6d7c18b1ff615496",
+    "url": "/eCommerce/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a4c0c146f4faddac46a9",
+    "url": "/eCommerce/static/css/main.91b1f042.chunk.css"
+  },
+  {
+    "revision": "a1ea6d7c18b1ff615496",
+    "url": "/eCommerce/static/js/2.e9ff46bc.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/eCommerce/static/js/2.e9ff46bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c0c146f4faddac46a9",
+    "url": "/eCommerce/static/js/main.3e8d5846.chunk.js"
+  },
+  {
+    "revision": "c61ad988e958c53b560b",
+    "url": "/eCommerce/static/js/runtime-main.b8c3ebdc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/eCommerce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/eCommerce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/eCommerce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/eCommerce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/eCommerce/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4560de661f44019842efb53f40a337ab",
+    "url": "/eCommerce/static/media/testProduct.4560de66.jpg"
+  }
+]);
