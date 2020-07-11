7c0261c832d8023e07cac6143f6f63f94d26f1a9
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-07-11 09:18:32 +0200

    deploy to heroku

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Router } from "react-router-dom";
 // import Home from "../pages/Home/Home";
 import Admin from "../../pages/Admin/Admin";
 import Products from "../../pages/Products/Products";
 import Cart from "../../pages/Cart/Cart";
 import ProductDetail from "../../pages/ProductDetail/ProductDetail";
 import User from "../../pages/User/User";
+
 
 function App() {
   return (
